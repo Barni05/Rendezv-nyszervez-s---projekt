44239489ef97dfd9687dae8914883cd079565976
mtlhdnrd <mtlhdnrd@protonmail.com>
2025-01-07 14:34:29 +0100

    merge conflict marker

--- Rendezvenyszervezes/Rendezvenyszervezes/Equipment.cs
+++ Rendezvenyszervezes/Rendezvenyszervezes/Equipment.cs
@@ -5,21 +5,12 @@ namespace Rendezvenyszervezes {
         int numberOfUses;
         int maintenanceFrequency;
         int size;
-<<<<<<< HEAD
         public Equipment(int _id, string _name, int _numberOfUses, int _maintenanceFreqeuency, int _size) {
             this.Id = _id;
             this.Name = _name;
             this.NumberOfUses = _numberOfUses;
             this.MaintenanceFrequency = _maintenanceFreqeuency;
             this.Size = _size;
-=======
-
-        public Equipment(string _name, int _size) {
-            this.name = _name;
-            this.numberOfUses = 0;
-            this.maintenanceFrequency = 1;
-            this.size = _size;
->>>>>>> 93835155349a75bc1da358ea54d0de795a84db75
         }
 
         public int Id { get => id; set => id = value; }
